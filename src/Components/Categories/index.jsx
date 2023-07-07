78b756411084d7f888f7dc93b766f0f2702abde7
nickmullaney <nick.mullaney@gmail.com>
2023-07-06 17:46:23 -0700

    as close as I can get

--- src/Components/Categories/index.jsx
+++ src/Components/Categories/index.jsx
@@ -1,7 +1,68 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { selectCategory, getCategories } from '../../Store/Categories';
+import { Box, Typography } from '@mui/material';
+import electronicsBackground from '../../../assets/images/electronicsBackground.jpg';
+import foodBackground from '../../../assets/images/foodBackground.jpg';
+import clothingBackground from '../../../assets/images/clothingBackground.jpg';
+import weaponsBackground from '../../../assets/images/weaponBackground.jpg';
+
 const Categories = () => {
+  const { categories } = useSelector(getCategories);
+  const dispatch = useDispatch();
+
+  const handleCategoryClick = (category) => {
+    dispatch(selectCategory(category));
+  };
+
+  const getCategoryBackground = (categoryName) => {
+    switch (categoryName) {
+      case 'electronics':
+        return `url(${electronicsBackground})`;
+      case 'food':
+        return `url(${foodBackground})`;
+      case 'clothing':
+        return `url(${clothingBackground})`;
+      case 'weapons':
+        return `url(${weaponsBackground})`;
+      default:
+        return 'none';
+    }
+  };
+
   return (
     <div>
-      <h1>Categories</h1>
+      <Typography variant="h4" component="h1" align="center">
+        Categories
+      </Typography>
+      <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 4 }}>
+        {categories.map((category) => (
+          <Box
+            key={category.id}
+            onClick={() => handleCategoryClick(category)}
+            sx={{
+              cursor: 'pointer',
+              border: '1px solid #ccc',
+              borderRadius: 4,
+              padding: 2,
+              marginRight: 2,
+              width: '150px', // Set a fixed width for all category boxes
+              backgroundImage: getCategoryBackground(category.name),// Apply the background image
+              textAlign: 'center',
+              fontSize: '1rem',
+              backgroundSize: 'cover',
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: 'center',
+              color: '#fff', // Set text color to white for better visibility
+              '& .MuiTypography-root': {
+                fontWeight: 'bold', // Make the text bold
+                fontSize: '1.1rem', // Increase the font size slightly
+              },
+            }}
+          >
+            <Typography variant="body1">{category.displayName}</Typography>
+          </Box>
+        ))}
+      </Box>
     </div>
   );
 }
---
+++ src/Store/Products/index.jsx
@@ -0,0 +1,30 @@
+// Reducer
+
+const initialState = {
+  products: [
+    { id: 1, category: 'electronics', name: 'TV', description: 'A 50 inch TV', price: 699.99 },
+    { id: 2, category: 'electronics', name: 'Radio', description: 'A radio', price: 99.99 },
+    { id: 3, category: 'food', name: 'Bread', description: 'A loaf of bread', price: 2.99 },
+    { id: 4, category: 'food', name: 'Eggs', description: 'A dozen eggs', price: 1.99 },
+    { id: 5, category: 'clothing', name: 'Shirt', description: 'A button up shirt', price: 9.99 },
+    { id: 6, category: 'clothing', name: 'Pants', description: 'A pair of pants', price: 19.99 },
+    { id: 7, category: 'weapons', name: 'Sword', description: 'A sword', price: 29.99 },
+    { id: 8, category: 'weapons', name: 'Axe', description: 'An axe', price: 39.99 },
+  ],
+};
+
+export const getProductsByCategory = (state, category) => {
+  if (category) {
+    return state.products.filter((product) => product.category === category.name);
+  }
+  return state.products;
+};
+
+const productsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default productsReducer;
